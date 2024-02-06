0b182b6c6eca66ba49aebe404d7c8c741a377e54
Vaishav Dhepe <vaishav@vaishavs-air.home>
2024-02-05 21:23:35 -0400

    added welcome screen, theme, colors

--- app/src/main/java/com/example/alumniconnect/ui/screens/welcome/Screen.kt
+++ app/src/main/java/com/example/alumniconnect/ui/screens/welcome/Screen.kt
@@ -1,15 +1,122 @@
 package com.example.alumniconnect.ui.screens.welcome
 
+import android.graphics.drawable.Drawable
+import android.util.Log
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Divider
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavBackStackEntry
 import com.example.alumniconnect.ui.theme.AlumniConnectTheme
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.alumniconnect.R
+
 
 @Composable
-fun WelcomeScreen(modifier: Modifier = Modifier) {
-    Text(text = "Ikade")
+fun WelcomeScreen(
+    modifier: Modifier = Modifier,
+    viewModel: WelcomeViewModel = viewModel()
+) {
+    val uiState by viewModel.uiState.collectAsState()
+    val context = LocalContext.current
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+            .fillMaxSize()
+            .background(color = Color(context.getColor(R.color.white)))
+    ) {
+        Spacer(modifier = modifier.height(100.dp))
+        Divider(
+            color = Color(context.getColor(R.color.primary_grey)),
+            thickness = 5.dp,
+            modifier = modifier
+                .padding(horizontal = 20.dp)
+        )
+        Text(
+            text = context.getString(R.string.app_name),
+            style = MaterialTheme.typography.displayMedium,
+            fontWeight = FontWeight.Bold,
+            modifier = modifier.padding(vertical = 15.dp)
+        )
+        Divider(
+            color = Color(context.getColor(R.color.primary_grey)),
+            thickness = 5.dp,
+            modifier = modifier
+                .padding(horizontal = 20.dp)
+        )
+        Text(
+            text = context.getString(R.string.welcome_desc),
+            style = MaterialTheme.typography.titleMedium,
+            textAlign = TextAlign.Center,
+            modifier = modifier
+                .width(250.dp)
+                .padding(vertical = 20.dp)
+        )
+        Box(
+            modifier = modifier
+                .size(270.dp)
+                .fillMaxSize()
+                .padding(20.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.welcome_connections),
+                contentDescription = null,
+                contentScale = ContentScale.FillBounds,
+                modifier = modifier.fillMaxSize()
+            )
+        }
+        Button(
+            onClick = { /*TODO*/ },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(0xFFf0f0f0),
+                contentColor = Color.Black
+            ),
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(start = 20.dp, top = 20.dp, end = 20.dp, bottom = 10.dp),
+            shape = RoundedCornerShape(10.dp)
+        ) {
+            Text(text = context.getString(R.string.btn_get_started))
+        }
+        ClickableText(text = AnnotatedString(context.getString(R.string.txt_btn_already_account)),
+            style = MaterialTheme.typography.bodySmall.merge(TextStyle(textDecoration = TextDecoration.Underline)),
+            onClick = { Log.d("ACCBTN", "Pressed") })
+    }
 }
 
 @Preview(showBackground = true)
--- app/src/main/java/com/example/alumniconnect/ui/theme/Theme.kt
+++ app/src/main/java/com/example/alumniconnect/ui/theme/Theme.kt
@@ -10,21 +10,23 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = md_theme_light_onSurfaceVariant,
+    secondary = seed,
+    tertiary = md_theme_light_scrim
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = seed,
+    secondary = md_theme_light_scrim,
+    tertiary = md_theme_light_onPrimary
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -36,6 +38,7 @@ private val LightColorScheme = lightColorScheme(
     onSurface = Color(0xFF1C1B1F),
     */
 )
+
 
 @Composable
 fun AlumniConnectTheme(
@@ -61,7 +64,16 @@ fun AlumniConnectTheme(
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
         }
     }
-
+    val systemUiController = rememberSystemUiController()
+    if (darkTheme) {
+        systemUiController.setSystemBarsColor(
+            color = Color.Transparent
+        )
+    } else {
+        systemUiController.setSystemBarsColor(
+            color = Color.Transparent
+        )
+    }
     MaterialTheme(
         colorScheme = colorScheme,
         typography = Typography,
